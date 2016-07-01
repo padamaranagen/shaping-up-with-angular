d6a39c99e95b601527d854fee15ca2262665aa8b
Nagendra Prasad <wowmylibrary@gmail.com>
2016-07-01 09:54:01 +0530

    Task #1 Completed.

--- public/1/app.js
+++ public/1/app.js
@@ -1,1 +1,8 @@
-﻿
+﻿(function () {
+    var gem = { name: 'Azurite', price: 2.95 };
+    var app = angular.module('gemStore', []);
+
+    app.controller('StoreController', function ($scope) {
+        this.product = gem;
+    });
+})();
