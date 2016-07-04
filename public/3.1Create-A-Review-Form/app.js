3aa920dffceefee810060053a569733854610669
Nagendra Prasad <wowmylibrary@gmail.com>
2016-07-04 11:58:20 +0530

    Review Date issues fixed

--- public/3.1Create-A-Review-Form/app.js
+++ public/3.1Create-A-Review-Form/app.js
@@ -23,10 +23,13 @@
             this.current = newGallery || 0;
         };
     });
+    
+
     //Added ReviewController
     app.controller('ReviewController', function () {
         this.review = {};
         this.addReview = function (product) {
+            this.review.createdOn = Date.now();
             product.reviews.push(this.review);
             this.review = {};
         };
