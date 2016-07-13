f7fafdd6fb4169283fb8b23905b9e70ac5092b21
Nagendra Prasad <nagendramca2011@gmail.com>
2016-07-13 16:36:15 +0530

    L5:T2 1. Pass our store controller the $http Service. 2. get the store-products.json using the $http Service. 3. Attach a success to our get call. 4. Name the first parameter of the success function data. 5. Give our StoreController access to the products by setting products equal to the data given to us with the http service's success promise.

--- public/L5/README.md
+++ public/L5/README.md
@@ -20,3 +20,11 @@
 3. Give gemStore Module access to the directives by adding a dependency to gemStore's definition.
 4. Link in the new products.js file.
 ```
+
+```
+1. Pass our store controller the $http Service.
+2. get the store-products.json using the $http Service.
+3. Attach a success to our get call.
+4. Name the first parameter of the success function data.
+5. Give our StoreController access to the products by setting products equal to the data given to us with the http service's success promise.
+```
--- public/L5/app.js
+++ public/L5/app.js
@@ -1,9 +1,13 @@
 ﻿(function () {
     var app = angular.module('gemStore', ['store-directives']);
 
-    app.controller('StoreController', function () {
-        this.products = gems;
-    });
+    app.controller('StoreController', ['$http', function ($http) {
+        var store = this;
+        store.products = [];
+        $http.get('store-products.json').success(function (data) {
+            store.products = data;
+        });
+    }]);
 
     app.controller("ReviewController", function () {
 
@@ -15,77 +19,5 @@
         };
 
     });
-
-    var gems = [{
-        name: 'Azurite',
-        description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
-        shine: 8,
-        price: 110.50,
-        rarity: 7,
-        color: '#CCC',
-        faces: 14,
-        images: [
-          "images/1.jpg",
-          "images/2.jpg",
-          "images/3.jpg"
-        ],
-        reviews: [{
-            stars: 5,
-            body: "I love this gem!",
-            author: "joe@example.org"
-        }, {
-            stars: 1,
-            body: "This gem sucks.",
-            author: "tim@example.org"
-        }]
-    }, {
-        name: 'Bloodstone',
-        description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
-        shine: 9,
-        price: 22.90,
-        rarity: 6,
-        color: '#EEE',
-        faces: 12,
-        images: [
-          "images/4.jpg",
-          "images/5.jpg",
-          "images/6.jpg"
-        ],
-        reviews: [{
-            stars: 3,
-            body: "I think this gem was just OK, could honestly use more shine, IMO.",
-            author: "JimmyDean@example.org"
-        }, {
-            stars: 4,
-            body: "Any gem with 12 faces is for me!",
-            author: "gemsRock@example.org"
-        }]
-    }, {
-        name: 'Zircon',
-        description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
-        shine: 70,
-        price: 1100,
-        rarity: 2,
-        color: '#000',
-        faces: 6,
-        images: [
-          "images/7.jpg",
-          "images/8.jpg",
-          "images/1.jpg"
-        ],
-        reviews: [{
-            stars: 1,
-            body: "This gem is WAY too expensive for its rarity value.",
-            author: "turtleguyy@example.org"
-        }, {
-            stars: 1,
-            body: "BBW: High Shine != High Quality.",
-            author: "LouisW407@example.org"
-        }, {
-            stars: 1,
-            body: "Don't waste your rubles!",
-            author: "nat@example.org"
-        }]
-    }];
-
+   
 })();
