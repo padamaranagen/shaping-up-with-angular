84a5b1b5bb32cb82a7363564ea3516d3bed9b89b
Nagendra Prasad <nagendramca2011@gmail.com>
2016-07-13 17:32:01 +0530

    L2: T1 1. Use an Angular filter to display the price as a currency.

---
+++ public/L2/README.md
@@ -0,0 +1,8 @@
+### Level2- Built-in Directives 
+
+
+#### Tasks
+
+```
+1. Use an Angular filter to display the price as a currency.
+```
---
+++ public/L2/app.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    var app = angular.module('gemStore', []);
+
+    app.controller('StoreController', function () {
+        this.products = gems;
+    });
+
+    var gems = [
+      { name: 'Azurite', price: 110.50 },
+      { name: 'Bloodstone', price: 22.90 },
+      { name: 'Zircon', price: 1100 },
+    ];
+})();
