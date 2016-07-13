b271ad8d17335425e7fd68ec0856641de5a6f6c0
Nagendra Prasad <nagendramca2011@gmail.com>
2016-07-13 15:54:58 +0530

    L5:T1 Completed!

--- public/L5/app.js
+++ public/L5/app.js
@@ -1,42 +1,8 @@
 ﻿(function () {
-    var app = angular.module('gemStore', []);
+    var app = angular.module('gemStore', ['store-directives']);
 
     app.controller('StoreController', function () {
         this.products = gems;
-    });
-
-    app.directive("productTabs", function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'product-tabs.html',
-            controller: function () {
-                this.tab = 1;
-
-                this.isSet = function (checkTab) {
-                    return this.tab === checkTab;
-                };
-
-                this.setTab = function (setTab) {
-                    this.tab = setTab;
-                };
-            },
-            controllerAs: 'tab'
-        };
-    });
-
-
-    app.directive("productGallery", function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'product-gallery.html',
-            controller: function () {
-                this.current = 0;
-                this.setCurrent = function (imageNumber) {
-                    this.current = imageNumber || 0;
-                };
-            },
-            controllerAs: 'gallery'
-        };
     });
 
     app.controller("ReviewController", function () {
@@ -48,27 +14,6 @@
             this.review = {};
         };
 
-    });
-
-    app.directive('productDescription', function () {
-        return {
-            restrict: 'E',//E for element
-            templateUrl: 'product-description.html'//Url for the template
-        };
-    });
-
-    app.directive("productSpecs", function () {
-        return {
-            restrict: 'A',
-            templateUrl: 'product-specs.html'
-        };
-    });
-
-    app.directive("productReviews", function () {
-        return {
-            restrict: 'E',
-            templateUrl:'product-reviews.html'
-        }
     });
 
     var gems = [{
